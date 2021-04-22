8f9b7bf87cc080560f821e98f22050b904339005
islam <rysbekov.i312@gmail.con>
2021-04-22 23:43:22 +0600

    not finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,63 +18,61 @@ public class Main {
                 {"ЛОНДОН", "0.5:2900.0->1.0:500.0"},
                 {"МАДРИД", "0.5:3000.0->1.0:700.0"},
         };
+        boolean a = true;
 
-        System.out.println("Введите вес груза: ");
-        float weight = scanner.nextFloat();
-
-        System.out.println("Доступные города:");
-        for (int i = 0; i < data.length; i++) {
-            System.out.println(i + ". " + data[i][0]);
-        }
-        System.out.println("Введите индекс города из списка:");
-
-        boolean whileTrue = true;
-        while (whileTrue) {
-
-
-            int citi_Id = scanner.nextInt();
-
-
-            if (citi_Id < 0 || citi_Id >= data.length && whileTrue) {
+        while (a) {
+            System.out.println("Введите вес груза: ");
+            float weight = scanner.nextFloat();
+            if (weight <= 0 && a) {
+                System.out.println("Введите правильный вес");
+            } else if (weight > 0 && a) {
+                System.out.println("Доступные города:");
+                for (int i = 0; i < data.length; i++) {
+                    System.out.println(i + ". " + data[i][0]);
+                }
                 System.out.println("Введите индекс города из списка:");
-            } else if (citi_Id < data.length && citi_Id >= 0) {
-                whileTrue = false;
+                boolean whileTrue = true;
+                while (whileTrue) {
+                    int citi_Id = scanner.nextInt();
+                    if (citi_Id < 0 || citi_Id >= data.length && whileTrue) {
+                        System.out.println("Введите индекс города из списка:");
+                    } else if (citi_Id < data.length && citi_Id >= 0) {
+                        whileTrue = false;
+                        String[] temp = data[citi_Id][1].split("->1.0:");
+                        float priceForEachKG = Float.parseFloat(temp[1]);
+                        temp = temp[0].split(";");
+                        for (int i = 0; i < temp.length; i++) {
+                            String[] intervals = temp[i].split(":");
+                            float intervalWeight = Float.parseFloat(intervals[0]);
+                            float priceInterval = Float.parseFloat(intervals[1]);
 
-                String[] temp = data[citi_Id][1].split("->1.0:");
-
-                float priceForEachKG = Float.parseFloat(temp[1]);
-
-                temp = temp[0].split(";");
-
-                for (int i = 0; i < temp.length; i++) {
-
-                    String[] priceInterval = temp[i].split(":");
-
-                    float price = Float.parseFloat(priceInterval[1]);
-                    float intervalWeight = Float.parseFloat(priceInterval[0]);
-                    if (weight <= intervalWeight) {
-                        System.out.println("Price = " + priceInterval);
-                        break;
-                    }
-                    else if (i == temp.length - 1) {
-                        float prices = ((weight - intervalWeight) * priceForEachKG + price);
-                        if (price <= 0) {
-                            System.out.println("Price can not be calculated");
-                        } else {
-                            System.out.println("Price: " + prices);
+                            if (weight <= intervalWeight) {
+                                System.out.println("Price = " + priceInterval);
+                                break;
+                            } else if (i == temp.length - 1) {
+                                float price = (weight - intervalWeight) * priceForEachKG + priceInterval;
+                                System.out.println("Price: " + price);
+                            }
                         }
                     }
-
-
                 }
             }
+            if (a) {
+                System.out.println("" +
+                        "Хотите оформить ещё заказ?\n" +
+                        "1. Да \n" +
+                        "2. Нет");
+                int f = scanner.nextInt();
+                if (f == 2) {
+                    a = false;
+                    System.out.println("Спасибо за заказ.");
 
+                } else if (f == 1) a = true;
+                else if (f > 2 && f <= 0) {
+                    System.out.println("Убедитесь в правильности набора");
+                    a = true;
+                }
+            }
         }
-
-               /* String[] weightInterval = temp[0].split(":");
-                String[] priceInterval = temp[1].split(":");
-                System.out.println(Arrays.toString(weightInterval));*/
     }
-
-
 }
